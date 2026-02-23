4a2df6fdb84e209051aee3e916d6e50a4a9322e1
HaswanthTamil <haswanthkumar278@gmail.com>
2026-02-23 10:13:27 +0530

    Trained a new model apnea_lgbm.pkl

--- apnea-ahi-estimator/src/train.py
+++ apnea-ahi-estimator/src/train.py
@@ -7,16 +7,14 @@ import lightgbm as lgb
 # Load dataset
 df = pd.read_csv("data/epochs_features.csv")
 
-# Split features / labels
 X = df.drop(columns=["label", "record"])
 y = df["label"]
 
-# Train/val split (stratified because classes are imbalanced)
+# Stratified split
 X_train, X_val, y_train, y_val = train_test_split(
     X, y, test_size=0.2, stratify=y, random_state=42
 )
 
-# Define model (stronger than baseline)
 model = lgb.LGBMClassifier(
     n_estimators=800,
     learning_rate=0.03,
@@ -29,7 +27,6 @@ model = lgb.LGBMClassifier(
     random_state=42
 )
 
-# Train
 model.fit(X_train, y_train)
 
 # Evaluate
